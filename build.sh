0ec5998e4606720bf4c72e9ebb1dc9c240cfaca2
Nigel <2674270+nigeldixon@users.noreply.github.com>
2025-03-06 21:03:57 +0000

    Add albert launcher

--- build.sh
+++ build.sh
@@ -19,6 +19,8 @@ set -ouex pipefail
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+rpm-ostree install albert
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
